e052041157682026c46a23234374c5ee159a7327
jazzhammer <tio.a@tacteonltd.com>
2023-09-11 16:01:37 -0700

    init

---
+++ crispy_api/crispy_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CrispyWebConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "crispy_web"
---
+++ crispy_api/crispy_web/mydb.py
@@ -0,0 +1,18 @@
+from psycopg2 import connect, extensions
+
+
+autocommit = extensions.ISOLATION_LEVEL_AUTOCOMMIT
+
+con = connect(
+    database='jitguru',
+    user='postgres',
+    host='localhost',
+    port='5432'
+)
+
+print(con.status)
+con.set_isolation_level(autocommit)
+cursor = con.cursor()
+# cursor.execute("SET AUTOCOMMIT = ON")
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS jitguru")
